ed2b7233438f7273e89841895a225f27bc2ad34f
Thimira Sahan <161017879+SahanJDesigns@users.noreply.github.com>
2025-08-03 21:58:22 +0530

    Delete start-dev.sh

--- start-dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Starting Genealogy Tree Creator in Development Mode..."
-echo "This will enable hot reload and mount source code volumes"
-echo ""
-docker-compose -f docker-compose.dev.yml up --build
